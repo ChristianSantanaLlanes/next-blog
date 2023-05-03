8e2c0dcaee15ebd3700f7e0873e0e93021eb882d
yuta.sano <snyt45@gmail.com>
2023-05-03 21:53:13 +0900

    update Header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,14 +1,53 @@
 "use client";
 
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Tooltip,
+  useColorMode,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { BLOG_NAME } from "src/lib/constants";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const ColorSwitchButton = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const tooltipLabel =
+    colorMode === "light"
+      ? "ダークモードへ切り替えます"
+      : "ライトモードへ切り替えます";
+  return (
+    <Tooltip label={tooltipLabel}>
+      <IconButton
+        aria-label="swith color mode"
+        icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        onClick={toggleColorMode}
+      />
+    </Tooltip>
+  );
+};
 
 export default function Header() {
   return (
-    <header>
-      <h1>
-        <Link href="/">{BLOG_NAME}</Link>
-      </h1>
-    </header>
+    <Box px={4}>
+      <Container maxW="container.lg">
+        <Flex
+          as="header"
+          py="4"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Link href="/" passHref>
+            <Heading as="h1" fontSize="2xl" cursor="pointer">
+              {BLOG_NAME}
+            </Heading>
+          </Link>
+          <ColorSwitchButton />
+        </Flex>
+      </Container>
+    </Box>
   );
 }
