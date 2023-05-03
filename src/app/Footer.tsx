b4efde6ecf47e5c0cbdf70d02cfb5dda34913d32
yuta.sano <snyt45@gmail.com>
2023-05-03 21:59:28 +0900

    update Footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import { BLOG_NAME } from "src/lib/constants";
 
 export default function Footer() {
   return (
-    <footer>
-      <small>{`© 2023 ${BLOG_NAME}`}</small>
-    </footer>
+    <Box px={4}>
+      <Container maxW="container.lg">
+        <Flex as="footer" py="4" justifyContent="center">
+          <Text fontSize="md">{`© 2023 ${BLOG_NAME}`}</Text>
+        </Flex>
+      </Container>
+    </Box>
   );
 }
